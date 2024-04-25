8ab859062a5260ddde4d6c741432117824d771ba
Alberto Sierra <alberto.sierra@murciaeduca.es>
2024-04-25 10:46:59 +0200

    Showing tests differences

--- actions/answers/getDiffWithSolution.php
+++ actions/answers/getDiffWithSolution.php
@@ -1,25 +1,21 @@
+<?php
+require_once "../../initTsugi.php";
+?>
 <html>
 	<body>
 		<h3>lines preceded by -: expected output</h3>
 		<h3>lines preceded by +: got output</h3>
 		<pre>
 <?php
-require_once "../../initTsugi.php";
-
-$exercise = new \CT\CT_Exercise($_GET['exerciseId']);
 $answer = new \CT\CT_Answer($_GET['answerId']);
-$exercise = new \CT\CT_ExerciseCode;
-$class =  get_class($exercise);
-if($class == "CT\CT_ExerciseCode") {
-	$solution = $exercise->getOutputFromCode(
-		$answer->getAnswerTxt(), $exercise->getExerciseLanguage(), $exercise->getExerciseInputGrade()
-	);
-	if ($USER->instructor) {
-		$answertxt = $exercise->getExerciseOutputGrade();
-	} else {
-		$answertxt = $exercise->getExerciseOutputTest();
+foreach ($answer->getTestsOutput() as $key => $testOutput) {
+	if(
+		$USER->instructor
+		|| $testOutput->visibleTest !== 0
+	) {
+		echo "Test: ".$key."\n";
+		echo \CT\CT_Answer::getDiffWithSolution($testOutput->expectedOutput, $testOutput->obtainedOutput);
 	}
-echo \CT\CT_Answer::getDiffWithSolution($solution, $answertxt);
 }
 ?>
 		</pre>
